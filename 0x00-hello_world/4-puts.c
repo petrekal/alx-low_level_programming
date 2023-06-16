14c6bc75cc664587ee8abe01b8bf3438a7acd7e5
petrekal <peterskullz@gmail.com>
2023-06-16 06:13:55 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - my first program using puts function
+ * Return: 0
+ */
+int main(void)
+{
+put("\"Programming is like building a multilingual puzzle");
+return (0);
+}
